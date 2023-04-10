f30155927669e962537a13ac5ef02671a52eb0bf
ZakariaPZ <zaaabc123@gmail.com>
2023-04-10 14:48:49 -0400

    Added VAE

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 from torch import nn
+import torch
 
+cuda_avail = torch.cuda.is_available()
+device = torch.device("cuda" if cuda_avail else "cpu")
+
+###########################################################################
 
 class Encoder(nn.Module):
     def __init__(self) -> None:
@@ -46,6 +51,60 @@ class Decoder(nn.Module):
         return self.decoder(x)
     
 
+    
+class VariationalEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.latent_dim = 8
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=2, padding=1), # 14 x 14 x 16
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),  # 7 x 7 x 32
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1), # 4 x 4 x 64
+            nn.ReLU(),
+            nn.Flatten()
+        )
+
+        self.mu = nn.Linear(4 * 4 * 64, self.latent_dim)
+        self.log_variance = nn.Linear(4 * 4 * 64, self.latent_dim)
+
+    def sample_noise(self):
+        return torch.randn(self.latent_dim)
+
+    def forward(self, x):
+        
+        x = self.encoder(x)
+        mu = self.mu(x)
+        log_variance = self.log_variance(x)
+        epsilon = self.sample_noise().to(device)
+        z = mu + torch.exp(0.5*log_variance) * epsilon
+
+        return z, mu, log_variance
+    
+###########################################################################
+
+class VAELoss(nn.Module):
+    def __init__(self):
+        super(VAELoss, self).__init__()
+        self.alpha = 1100
+
+    def kl_loss(self, mu, log_variance):
+        loss = -0.5 * torch.sum(1 + log_variance - torch.square(mu) - torch.exp(log_variance), dim=1)
+        return loss
+
+    def reconstruction_loss(self, y_true, y_pred):
+        loss = torch.mean(torch.square(y_true - y_pred), dim=(1, 2, 3))
+        return loss
+
+    def forward(self, predictions, targets, mu, log_variance):
+        kld = self.kl_loss(mu, log_variance)
+        reconstruction = self.reconstruction_loss(targets, predictions)
+        return torch.sum(kld + self.alpha*reconstruction)
+
+###########################################################################
 
 class Autoencoder(nn.Module):
     def __init__(self) -> None:
@@ -58,3 +117,17 @@ class Autoencoder(nn.Module):
         x = self.encoder(x) 
         x = self.decoder(x)
         return x
+    
+class VariationalAutoencoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.encoder = VariationalEncoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        x, mu, log_variance = self.encoder(x) 
+        x = self.decoder(x)
+        return x, mu, log_variance
+    
+###########################################################################
