3813a3aed833827a395314fe17dbfca3215215c0
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2023-04-12 19:45:54 -0400

    Add command line arguments

--- ae_test.py
+++ ae_test.py
@@ -1,3 +1,4 @@
+import argparse
 from typing import *
 
 import matplotlib.pyplot as plt
@@ -7,54 +8,178 @@ import torch.nn as nn
 import torch.nn.functional as F
 from ae import DenseAE
 from torch.utils.data import DataLoader, random_split
-from torchmetrics import Accuracy, ConfusionMatrix
 from torchmetrics.functional import confusion_matrix
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
-BATCH_SIZE = 256
-TRAIN_DIGIT = 8
-COMPARE_DIGIT = 7
-VAL_SIZE = 0.5
-
 if __name__ == '__main__':
     pl.seed_everything(1)
 
-    model = DenseAE(
-        input_dim = 28*28,
-        latent_dim = 32,
-        n_layers_encoder = 2,
-        n_layers_decoder = 2,
-        encoder_width = 784,
-        decoder_width = 784,
-        scaling_factor = 1/2,
-        norm = 'batch',
-        dropout = 0.3,
-        normal_class=TRAIN_DIGIT
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--type',
+        type=str,
+        default='dense',
+        choices=['dense', 'conv'],
+        help='type of autoencoder to use',
     )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='MNIST',
+        choices=['MNIST', 'CIFAR10'],
+        help='dataset to use',
+    )
+    parser.add_argument(
+        '--latent_dim',
+        type=int,
+        default=32,
+        help='latent dimension of the autoencoder',
+    )
+    parser.add_argument(
+        '--n_layers_encoder',
+        type=int,
+        default=2,
+        help='number of layers in the encoder (not including the first and last linear map layers)',
+    )
+    parser.add_argument(
+        '--n_layers_decoder',
+        type=int,
+        default=2,
+        help='number of layers in the decoder (not including the first and last linear map layers)',
+    )
+    parser.add_argument(
+        '--encoder_width',
+        type=int,
+        default=784,
+        help='width of the first encoder layer after the input layer',
+    )
+    parser.add_argument(
+        '--decoder_width',
+        type=int,
+        default=784,
+        help='width of the last decoder layer before the output layer',
+    )
+    parser.add_argument(
+        '--scaling_factor',
+        type=float,
+        default=1/2,
+        help='scaling factor for the width of the encoder and decoder layers',
+    )
+    parser.add_argument(
+        '--norm',
+        type=str,
+        default='batch',
+        help='normalization to use in the encoder and decoder',
+    )
+    parser.add_argument(
+        '--dropout',
+        default=0.3,
+        help='dropout to use in the encoder and decoder',
+    )
+    parser.add_argument(
+        '--normal_class',
+        type=int,
+        default=1,
+        help='class to use as the normal class',
+    )
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=256,
+        help='batch size to use for training',
+    )
+    parser.add_argument(
+        '--val_size',
+        type=float,
+        default=0.5,
+        help='proportion of the test set to use as the validation set',
+    )
+    parser.add_argument(
+        '--lr',
+        type=float,
+        default=1e-3,
+        help='learning rate to use for training',
+    )
+    parser.add_argument(
+        '--eval_class',
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        '--checkpoint',
+        type=bool,
+        action='store_true',
+    )
+
+    args = parser.parse_args()
+
+    # check norm and dropout args
+    if args.norm not in ['batch', 'layer', 'None']:
+        raise ValueError(f'Invalid norm: {args.norm}')
+    else:
+        args.norm = None if args.norm == 'None' else args.norm
+    
+    if args.dropout == 'None':
+        args.dropout = None
+    else:
+        args.dropout = float(args.dropout)
+        if args.dropout < 0 or args.dropout > 1:
+            raise ValueError(f'Invalid dropout: {args.dropout}')
+
+  
+    if args.type == 'dense':
+
+        if args.dataset == 'MNIST':
+            input_dim = 28 * 28
+        else:
+            input_dim = 32 * 32 * 3
+
+        model = DenseAE(
+            input_dim = input_dim,
+            latent_dim = args.latent_dim,
+            n_layers_encoder = args.n_layers_encoder,
+            n_layers_decoder = args.n_layers_decoder,
+            encoder_width = args.encoder_width,
+            decoder_width = args.decoder_width,
+            scaling_factor = args.scaling_factor,
+            norm = args.norm,
+            dropout = args.dropout,
+            normal_class=args.normal_class,
+            lr=args.lr,
+        )
+    else:
+        raise NotImplementedError
+
+    # TODO: add handling for CIFAR10
 
     train = MNIST('./data', train=True, download=True, transform=transforms.ToTensor())
     # only use the digit we want
-    train_idx = torch.where(train.targets == TRAIN_DIGIT)[0]
+    train_idx = torch.where(train.targets == args.normal_class)[0]
     train.targets = train.targets[train_idx]
     train.data = train.data[train_idx]
-    train_loader = DataLoader(train, batch_size=BATCH_SIZE, num_workers=2, shuffle=True)
+    train_loader = DataLoader(train, batch_size=args.batch_size, num_workers=2, shuffle=True)
     
 
     test = MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
     # split the dataset into train and val
-    n_val = int(len(test) * VAL_SIZE)
+    n_val = int(len(test) * args.val_size)
     n_test = len(test) - n_val
     test, val = random_split(test, [n_test, n_val])
-    test_loader = DataLoader(test, batch_size=BATCH_SIZE, num_workers=2, shuffle=False)
-    val_loader = DataLoader(val, batch_size=BATCH_SIZE, num_workers=2, shuffle=False)
+    test_loader = DataLoader(test, batch_size=args.batch_size, num_workers=2, shuffle=False)
+    val_loader = DataLoader(val, batch_size=args.batch_size, num_workers=2, shuffle=False)
 
     trainer = pl.Trainer(
         accelerator='auto',
         devices='auto',
-        max_epochs=10,
+        max_epochs=args.epochs,
         check_val_every_n_epoch=1,
-        enable_checkpointing=False,
+        enable_checkpointing=args.checkpoint,
         log_every_n_steps=10
     )
 
@@ -64,7 +189,7 @@ if __name__ == '__main__':
     batch = next(iter(test_loader))
     model.eval()
 
-    idx = torch.where(batch[1] == TRAIN_DIGIT)[0][0]
+    idx = torch.where(batch[1] == args.normal_class)[0][0]
 
     plt.subplot(121)
     plt.imshow(batch[0][idx].squeeze(), cmap='gray')
@@ -73,10 +198,10 @@ if __name__ == '__main__':
     plt.subplot(122)
     plt.imshow(model(batch[0][idx]).squeeze().reshape(batch[0][0].shape[1], -1).detach().numpy(), cmap='gray')
     plt.title(f'Reconstruction: {batch[1][idx]}')
-    plt.savefig(f'reconstruction{TRAIN_DIGIT}.png', bbox_inches='tight')
+    plt.savefig(f'reconstruction{args.normal_class}.png', bbox_inches='tight')
     plt.show()
 
-    idx = torch.where(batch[1] == COMPARE_DIGIT)[0][0]
+    idx = torch.where(batch[1] == args.eval_class)[0][0]
 
     plt.subplot(121)
     plt.imshow(batch[0][idx].squeeze(), cmap='gray')
@@ -85,7 +210,7 @@ if __name__ == '__main__':
     plt.subplot(122)
     plt.imshow(model(batch[0][idx]).squeeze().reshape(batch[0][0].shape[1], -1).detach().numpy(), cmap='gray')
     plt.title(f'Reconstruction: {batch[1][idx]}')
-    plt.savefig(f'reconstruction{COMPARE_DIGIT}.png', bbox_inches='tight')
+    plt.savefig(f'reconstruction{args.eval_class}.png', bbox_inches='tight')
     plt.show()
 
     # for each batch in the test set, calculate the confusion matrix
