ef030e8f740ca04041d684c8ee96e76a9689cbff
ZakariaPZ <zaaabc123@gmail.com>
2023-04-12 01:35:49 -0400

    Added flexible conv AE

--- ae.py
+++ ae.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from typing import *
 
+
 class DenseAE(nn.Module):
     def __init__(self,
                  input_dim: int,
@@ -77,6 +78,116 @@ class DenseAE(nn.Module):
         return self.decoder(latent_repr)
     
 
+
+class Reshape(nn.Module):
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+    
+        return x.view(self.shape)
+
+class ConvAE(nn.Module):
+    def __init__(self,
+                 input_channels: int,
+                 H : int,
+                 latent_dim: int,
+                 n_layers_encoder: int,
+                 n_layers_decoder: int,
+                 encoder_width: int,
+                 decoder_width: int,
+                 scaling_factor: int = 1/2.,
+                 norm: Union[str, None] = None,
+                 dropout: Union[float, None] = None,
+                 padding : int = None,
+                 kernel_size : int = None,
+                 pool_kernel : int = None,
+                 type: str = 'dense',
+                ):
+        super().__init__()
+
+        self.input_dim = input_channels
+        self.latent_dim = latent_dim
+        self.n_layers_encoder = n_layers_encoder
+        self.n_layers_decoder = n_layers_decoder
+        self.encoder_width = encoder_width
+        self.decoder_width = decoder_width
+        self.scaling_factor = scaling_factor 
+        self.norm = norm
+        self.dropout = dropout
+        self.padding = padding
+        self.kernel_size = kernel_size
+        self.pool_kernel = int(pool_kernel) # convert kernel to int if float is passed
+        self.static = False
+        self.block = ConvBlock
+        self.H = H
+
+        # check that the scaling factor is valid
+        if not (0 < scaling_factor < 1):
+            raise ValueError(f'Invalid scaling factor: {scaling_factor}')
+        
+        # check if the number of layers, width and scaling factor are compatabile
+        if encoder_width * scaling_factor ** (n_layers_encoder - 1) % 1 != 0:
+            raise ValueError(f'Invalid combination of encoder params: {encoder_width * scaling_factor ** (n_layers_encoder - 1)}')
+        if decoder_width * (1/scaling_factor) ** (n_layers_decoder - 1) % 1 != 0:
+            raise ValueError(f'Invalid combination of decoder params: {decoder_width * (1/scaling_factor) ** (n_layers_decoder - 1)}')
+        
+        # Encoder
+        blocks = []
+        for i in range(self.n_layers_encoder):
+            in_channels = input_channels if i == 0 else int(encoder_width * scaling_factor ** (self.n_layers_encoder - i))
+            out_channels = int(encoder_width * scaling_factor ** (self.n_layers_encoder - (i + 1)))
+            blocks.append(
+                self.block(
+                    in_channels,
+                    out_channels,
+                    norm=norm, dropout=dropout,
+                    conv_type='downsample'
+                )
+            )
+     
+        blocks.append(nn.Flatten())
+        num_channels = self.encoder_width
+        dims = int(H*(1/self.pool_kernel)**(self.n_layers_encoder))
+        blocks.append(nn.Linear(num_channels * dims * dims, self.latent_dim))
+        self.encoder = nn.Sequential(*blocks)
+
+        # Decoder
+        decoder_dim = round(self.H/2**self.n_layers_decoder)
+        blocks_dec = [nn.Linear(self.latent_dim, num_channels * decoder_dim * decoder_dim), 
+                  Reshape(-1, num_channels, decoder_dim, decoder_dim)] 
+        for i in range(self.n_layers_decoder):
+            H_in = round(H*(1/self.pool_kernel)**(self.n_layers_decoder - i))
+            H_out = round(H*(1/self.pool_kernel)**(self.n_layers_decoder - (i+1)))
+            stride = 2 # 2 for upsampling with conv_transpose
+            padding = self.padding
+            dilation = 1
+            kernel_size = self.kernel_size
+            output_padding = H_out - ((H_in - 1) * stride - 2 * padding + dilation * (kernel_size - 1) + 1)
+
+            in_channels = num_channels if i == 0 else int(decoder_width * scaling_factor ** (i))
+            out_channels = input_channels if i == self.n_layers_decoder - 1 else int(decoder_width * scaling_factor ** (i + 1))
+
+            blocks_dec.append(
+                self.block(
+                    in_channels,
+                    out_channels,
+                    norm=norm, dropout=dropout, output_padding=output_padding,
+                    conv_type='upsample',
+                    stride=stride,
+                    static=self.static
+                )
+            )
+
+        self.decoder = nn.Sequential(*blocks_dec)
+
+    def forward(self, input):
+        latent_repr = self.encoder(input)
+        output = self.decoder(latent_repr)
+        return output
+        
+
 class MLPBlock(nn.Module):
     def __init__(self,
                  input_dim: int,
@@ -108,12 +219,64 @@ class MLPBlock(nn.Module):
         if self.dropout:
             output = self.dropout(output)
         return output
-    
+
+
 class ConvBlock(nn.Module):
-    ...
+    def __init__(self, 
+                 input_dim : int,
+                 output_dim : int,
+                 kernel_size : int = 3,
+                 stride : int = 1,
+                 padding : int = 1,
+                 activation : Callable[[torch.Tensor], torch.Tensor] = nn.ReLU,
+                 conv_type : str = 'downsample',
+                 static : bool = False,
+                 output_padding : int = 0,
+                 norm = None,
+                 dropout = None) -> None:
+        super().__init__()
+
+        self.conv_type = conv_type
+        self.output_padding = output_padding
+
+        if conv_type == 'downsample':
+
+            self.conv = nn.Sequential(
+                nn.Conv2d(input_dim, 
+                          output_dim, 
+                          kernel_size=kernel_size, 
+                          padding=padding),
+                activation(),
+                nn.MaxPool2d(2)
+            )
+
+        else:
+            self.conv = nn.Sequential(
+                nn.ConvTranspose2d(input_dim,
+                                output_dim,
+                                kernel_size=kernel_size,
+                                padding=1,
+                                stride=2,
+                                output_padding=output_padding),
+                activation()
+            )
+ 
+        self.norm = nn.BatchNorm2d(output_dim) if norm else None
+        self.dropout = nn.Dropout(dropout) if dropout else None
+
+    def forward(self, x):
+
+        x = self.conv(x)
+        if self.norm:
+            x = self.norm(x)
+        if self.dropout:
+            x = self.dropout(x)
+        return x
+
+
 
 if __name__ == "__main__":
-    args ={
+    MLP_args ={
         'input_dim' : [28*28] * 12,
         "n_layers_encoder" : [2,2,2,1,1,1,2,2,2,4,4,4,],
         'encoder_width' : [392,784,1568,784,784,784,784,784,784,784,784,784],
@@ -125,11 +288,33 @@ if __name__ == "__main__":
         'dropout' : [0.3] * 12,
     }
 
+
+    CNN_args ={
+        'input_channels' : [1] * 12,
+        'H' : [28] * 12,
+        "n_layers_encoder" : [2,2,2,1,1,1,2,2,2,4,4,4,],
+        'encoder_width' : [4,16,64] + [16]*9,
+        'n_layers_decoder' : [2,2,2,1,2,4,1,2,4,1,2,4,],
+        'decoder_width' : [4,16,64] + [16]*9,
+        'latent_dim' : [32] * 12,
+        'scaling_factor' : [1/2] * 12,
+        'norm' : ['batch'] * 12,
+        'dropout' : [0.3] * 12,
+        'padding' : [1] * 12,
+        'kernel_size' : [3] * 12,
+        'pool_kernel' : [2] * 12,
+        'type' : ['cnn'] * 12
+    }
+
+
     for i in range(12):
-        args_ = {k: v[i] for k, v in args.items()}
+        args_ = {k: v[i] for k, v in CNN_args.items()}
         print(args_)
-        model = DenseAE(**args_)
-        input = torch.randn(2, args_['input_dim'])
+        # model = DenseAE(**args_)
+        model = ConvAE(**args_)
+        # input = torch.randn(2, args_['input_dim'])
+        input = torch.randn(2, 1, 28, 28)
         output = model(input)
+        print(output.shape)
         assert output.shape == input.shape, "Autoencoder output shape does not match input shape"
         
