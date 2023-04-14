7b6c97e90fdbe4d925fbb955286a503f836e720a
Vahid Zehtab <33608325+vahidzee@users.noreply.github.com>
2023-04-13 22:20:17 -0400

    add main trainer

---
+++ main.py
@@ -0,0 +1,19 @@
+from lightning.pytorch.cli import LightningCLI
+import lightning
+from importlib.metadata import version
+
+
+def main():
+    lightning.seed_everything(666)  # for reproducibility
+    print(f"lightning_toolbox version {version('lightning-toolbox')} is installed.")
+    LightningCLI(
+        lightning.LightningModule,
+        lightning.LightningDataModule,
+        subclass_mode_model=True,
+        subclass_mode_data=True,
+        auto_configure_optimizers=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
